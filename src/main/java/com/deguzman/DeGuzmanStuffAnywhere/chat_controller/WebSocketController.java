60c077987118ad2858e411e9e95a943592c9a56c
Ejdeguzman72 <ejdeguzman72@gmail.com>
2022-04-06 17:10:39 -0400

    put back chat package

---
+++ src/main/java/com/deguzman/DeGuzmanStuffAnywhere/chat_controller/WebSocketController.java
@@ -0,0 +1,22 @@
+package com.deguzman.DeGuzmanStuffAnywhere.chat_controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.deguzman.DeGuzmanStuffAnywhere.chat_model.MessageBean;
+
+@Controller
+@CrossOrigin
+public class WebSocketController {
+	
+	@MessageMapping("/user-all")
+	@SendTo("/topic/user")
+	public MessageBean sendToAll(@Payload MessageBean message) {
+		return message;
+	}
+	
+}
