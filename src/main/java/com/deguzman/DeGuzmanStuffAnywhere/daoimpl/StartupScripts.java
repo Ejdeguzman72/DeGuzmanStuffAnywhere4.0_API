eb464991e06ae3650aee8952222a4ee2c44168e1
Ejdeguzman72 <ejdeguzman72@gmail.com>
2022-04-18 23:37:57 -0400

    more changes

---
+++ src/main/java/com/deguzman/DeGuzmanStuffAnywhere/daoimpl/StartupScripts.java
@@ -0,0 +1,77 @@
+package com.deguzman.DeGuzmanStuffAnywhere.daoimpl;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.deguzman.DeGuzmanStuffAnywhere.model.ExerciseType;
+import com.deguzman.DeGuzmanStuffAnywhere.model.RestaurantType;
+import com.deguzman.DeGuzmanStuffAnywhere.model.TransactionType;
+
+@Repository
+public class StartupScripts {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired 
+	private ExerciseTypeDaoImpl exerciseTypeDaoImpl;
+	
+	@Autowired
+	private RestaurantTypeDaoImpl restaurantTypeDaoImpl;
+	
+	@Autowired 
+	private TransactionTypeDaoImpl transactionTypeDaoImpl;
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(StartupScripts.class);
+	
+	public String INSERT_EXERCISE_TYPES = "INSERT INTO EXERCISE_TYPES(EXERCISE_TYPE_NAME) VALUES('Abs', 'Back', 'Biceps', 'Chest', 'Delts','Legs', 'Shoulders','Triceps')";
+	
+	public String INSERT_RESTAURANT_TYPES = "INSERT INTO RESTAURANT_TYPE (DESCR) VALUES ('AMERICAN','PIZZERIA','CHINESE','HISPANIC','TEXMEX','BREAKFEAST','PASTA','STEAKHOUSE','SUSHI'','RAMEN','PHO','FAST FOOD')";
+	
+	public String INSERT_TRANSACTION_TYPES = "INSERT INTO TRANSACTION_TYPE(TRANSACTION_TYPE_DESCR) VALUES('RENT','GAS','CABLE','ELECTRIC','INTERNET','PHONES','INTERNET','MORTGAGE','INSURANCE - HOMEOWNERS','INSRUANCE - AUTOMOBILE','INSRUANCE - LIFE','GROCERIES','RESTAURANT','SCHOOL SUPPLIES','SOCIAL','VACATION','AUTO - OIL CHANGE','AUTO - BRAKES','AUTO - BODY WORK','AUTO - TIRES','AUTO - CAR DETAILS','AUTO - GENEAL MAINTENANCE',' AUTO - CRASH MAINTENANCE','MEDICAL - COPAY','MEDICAL - PRESCRIPTION', 'MEDICAL - HOSPITAL BILLS')";
+	
+	public int startUpExerciseTypes() {
+		List<ExerciseType> list = exerciseTypeDaoImpl.findAllExerciseTypeInformation();
+		int result = 0;
+		
+		if (list.size() == 0) {
+			result = jdbcTemplate.update(INSERT_EXERCISE_TYPES);
+			
+			LOGGER.info("List is empty, inserting records now...");
+		}
+		
+		return result;
+	}
+	
+	public int startupRestaurantTypes() {
+		List<RestaurantType> list = restaurantTypeDaoImpl.findAllRestaurantTypeInformation();
+		int result = 0;
+		
+		if (list.size() == 0) {
+			result = jdbcTemplate.update(INSERT_RESTAURANT_TYPES);
+			
+			LOGGER.info("List is empty, inserting records now...");
+		}
+		
+		return result;
+	}
+	
+	public int startupTransactionTypes() {
+		List<TransactionType> list = transactionTypeDaoImpl.retrieveAllTransactionTypes();
+		int result = 0;
+		
+		if (list.size() == 0) {	
+			result = jdbcTemplate.update(INSERT_TRANSACTION_TYPES);
+			
+			LOGGER.info("List is empty, inserting records now...");
+		}
+		
+		return result;
+	}
+	
+}
--- src/main/java/com/deguzman/DeGuzmanStuffAnywhere/event/AppStartupConfigEvent.java
+++ src/main/java/com/deguzman/DeGuzmanStuffAnywhere/event/AppStartupConfigEvent.java
@@ -2,18 +2,28 @@ package com.deguzman.DeGuzmanStuffAnywhere.event;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import com.deguzman.DeGuzmanStuffAnywhere.daoimpl.StartupScripts;
 
 @Component
 public class AppStartupConfigEvent {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AppStartupConfigEvent.class);
 
+	@Autowired
+	private StartupScripts startupScripts;
+	
 	@EventListener(ApplicationReadyEvent.class)
 	public void startApp() {
 
 		LOGGER.info("AppStartupConfigEvent - startApp(): Application has started on port 8080");
+		
+		startupScripts.startUpExerciseTypes();
+		startupScripts.startupRestaurantTypes();
+		startupScripts.startupTransactionTypes();
 	}
 }
